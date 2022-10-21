32247fbc7c2e1b15c037613e0c7e35d1f35da4a1
Jesse Johnson <oss@fictiontoreality.dev>
2022-10-21 11:26:00 -0700

    Add TODOs to reassess defaults in future.

--- lib/defaults.dart
+++ lib/defaults.dart
@@ -1,5 +1,11 @@
 import 'package:cryptography/cryptography.dart';
 
-final defaultGenerator = BigInt.from(2);
+/// Balance between compatibility, speed and security.
+/// FIXME: Should this be bumped up?
 final defaultHashAlgorithm = Sha256();
+
+/// TODO: Are there better (performance, security) generators?
+final defaultGenerator = BigInt.from(2);
+/// Source: 1024 bit prime from RFC 5054 Appendix A section 1.
+/// FIXME: Choose a better default safe prime!!! This one was chosen primarily for
 final defaultSafePrime = BigInt.parse('EEAF0AB9ADB38DD69C33F80AFA8FC5E86072618775FF3C0B9EA2314C9C256576D674DF7496EA81D3383B4813D692C6E0E0D5D8E250B98BE48E495C1D6089DAD15DC7D7B46154D6B6CE8EF4AD69B15D4982559B297BCF1885C529F566660E57EC68EDBC3C05726CC02FD4CBF4976EAA9AFD5138FE8376435B9FC61D2FC0EB06E3', radix: 16);
